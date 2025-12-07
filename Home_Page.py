0e96cb0523a40980b0a826692462f39d0a8ef7af
Preston Konkel <80935110+konkelmp@users.noreply.github.com>
2025-12-06 19:18:05 -0700

    Implement caching for FIRMS data in Home_Page.py
    Added caching for FIRMS data retrieval with a 24-hour TTL.

--- Home_Page.py
+++ Home_Page.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(
     page_title="My Streamlit Site",
@@ -17,3 +18,21 @@ st.markdown(
 )
 
 st.caption("Built with Streamlit")
+
+# Cache FIRMS data for 24hrs
+# FIRMS data contains last 3 days
+@st.cache_data(ttl=86400)
+def get_firms_data():            
+    firms_url = f"https://firms.modaps.eosdis.nasa.gov/api/area/csv/26af21577de6312527a09da2b7b3a18c/VIIRS_SNPP_NRT/world/3"
+    try:
+        firms_df = pd.read_csv(firms_url)
+        os.makedirs("data", exist_ok=True)
+        firms_df.to_csv(DATA_PATH, index=False)
+        return firms_df
+    except Exception as e:
+        st.error("Failed to fetch FIRMS data.")
+        return pd.DataFrame()
+
+if "firms_df" not in st.session_state:
+    st.session_state.firms_df = get_firms_data()
+firms_df = st.session_state.firms_df
